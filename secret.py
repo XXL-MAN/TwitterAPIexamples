0af181409feb72130d9b8bbde01dd5171ad098c6
@TheXXLMAN <gorriato@yahoo.es>
2023-01-08 12:01:51 +0100

    Create secret.py

---
+++ secret.py
@@ -0,0 +1,5 @@
+# Reemplaza estos valores con tus claves de API de Twitter
+consumer_key = 'TU_CONSUMER_KEY'
+consumer_secret = 'TU_CONSUMER_SECRET'
+access_token = 'TU_ACCESS_TOKEN'
+access_token_secret = 'TU_ACCESS_TOKEN_SECRET'
